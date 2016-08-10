4423e4f0613ce276e41672558d98eba03eaf239e
esickert <esickert@gmail.com>
2016-08-09 18:17:20 -0700

    more

--- src/webElementsActionsPackage/GetAttributes.java
+++ src/webElementsActionsPackage/GetAttributes.java
@@ -15,7 +15,7 @@ public class GetAttributes {
 	driver1.get("http://www.google.com");
 		System.out.println(date + " Successfully opened Google");
 
-		WebElement searchButton = driver1.findElement(By.name("btnK"));
+		WebElement searchButton = driver1.findElement(By.name("btnK"));  //this identifies the search button!!!
 		System.out.println("The name of the button is: " + searchButton.getAttribute("name"));
 		System.out.println("The name of the value is: " + searchButton.getAttribute("value"));
 		System.out.println("The name of the aira-label is: " + searchButton.getAttribute("aria-label"));
@@ -28,8 +28,9 @@ public class GetAttributes {
 		else 
 			System.out.println("The attributes are equal = f**king false");
 		
-		
-		
+/** 
+ * this is just a memory kick for me on ArrayList syntax
+ */		
 		ArrayList<Integer> test = new ArrayList<Integer>(); 
 		for(int i =0; i <= 10;i++)	{
 			test.add(i);
