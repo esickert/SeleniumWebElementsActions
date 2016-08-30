455d5c564ac9f01903c79282107ce6e25cabe610
esickert <esickert@gmail.com>
2016-08-29 18:27:26 -0700

    more

--- src/webElementsActionsPackage/SendKeys.java
+++ src/webElementsActionsPackage/SendKeys.java
@@ -13,9 +13,12 @@ public class SendKeys extends ExtraStuffActionsTest	{
 		WebDriver tom = new FirefoxDriver();
 		tom.get("http://www.yahoo.com");
 		WebElement sam = tom.findElement(By.name("p"));
-		sam.sendKeys(Keys.chord(Keys.SHIFT,"apple corp"));  //turns lower case to upper case
+		sam.sendKeys(Keys.chord(Keys.SHIFT,"apple corp"));  //turns lower case to upper case (hold shift while typing "apple corp")
 		toSleep();
 		sam.sendKeys(Keys.ENTER);  //sends ENTER to element p
 	}
+//the following code will hold down the ctrl key while typing "a"	
+//	String selectAll = Keys.chord(Keys.CONTROL, "a");
+//  driver.findElement(By.whatever("anything")).sendKeys(selectAll);
 
 }
