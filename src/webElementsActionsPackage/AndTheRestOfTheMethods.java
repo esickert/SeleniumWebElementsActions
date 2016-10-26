935190058337bb20426c9aaaf6c8635c23a58d69
esickert <esickert@gmail.com>
2016-10-25 17:45:18 -0700

    more

--- src/webElementsActionsPackage/AndTheRestOfTheMethods.java
+++ src/webElementsActionsPackage/AndTheRestOfTheMethods.java
@@ -5,12 +5,20 @@ import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 
-public class AndTheRestOfTheMethods {
+public class AndTheRestOfTheMethods extends ExtraStuffActionsTest	{
 
 	public void theRestOfTheMethods()	{
 	
 		WebDriver erich = new FirefoxDriver();
-		erich.get("https://www.google.com");
+		erich.get("http://www.google.com");
+		
+		WebElement search = erich.findElement(By.className("gb_P")); 
+		System.out.println(search.getText());      // OUTPUT: Gmail
+		System.out.println(search.getLocation());  // OUTPUT: (748, 19)
+		System.out.println(search.getTagName());   // OUTPUT: "a" refers to anchor but I don't remember what this refers to.
+		System.out.println(search.isDisplayed());  //returns a boolean OUTPUT: true/false
+		
+		toSleep();
 		
 	}
 		
